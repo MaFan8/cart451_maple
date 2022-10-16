10b95f059c1b04a23fc3f88ea9bde6e413539f59
MaFan8 <maple_f@hotmail.com>
2022-10-16 09:13:00 -0400

    added main.js, main.css

---
+++ maple/js/main.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    let reflections = document.getElementById('reflections');
+    let exercises = document.getElementById('exercises');
+    let proposal = document.getElementById('proposal');
+    let final = document.getElementById('final');
+    let clickCount =0;
+
+//     // CLICK PROJECT
+// document.getElementById('proposal').addEventListener('click', function() {
+//     if (clickCount%2==0) {
+//     windUp.style.visibility = "hidden";
+//     // exercises.style.visibility = "hidden";
+//     // final.style.visibility = "hidden";
+//     // //unhide lists
+//     // document.getElementById('proposal_box').style.display = "block";
+//     // document.getElementById('proposal_mid_grid').style.display = "block";
+//     // document.getElementById('final_box').style.display = "block";
+//   }
+//   else {
+//     windUp.style.visibility = "visible";
+//     // reflections.style.visibility = "visible";
+//     // presentation.style.visibility = "visible";
+//     // //unhide lists
+//     // document.getElementById('proposal_box').style.display = "none";
+//     // document.getElementById('proposal_mid_grid').style.display = "none";
+//     // document.getElementById('final_box').style.display = "none";
+//   }
+//     clickCount = clickCount+1;
+//   }); // END CLICK
+
+
+} //END ONLOAD
