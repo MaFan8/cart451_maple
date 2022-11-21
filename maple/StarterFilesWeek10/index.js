568cb25eec3f08adb2ca063ac8d92c562a644862
MaFan8 <maple_f@hotmail.com>
2022-11-21 11:16:36 -0500

    Added gardening, children txt docs

--- maple/StarterFilesWeek10/index.js
+++ maple/StarterFilesWeek10/index.js
@@ -13,7 +13,7 @@ let WordCount= require('./wordCount');
 let fs = require('fs');
 
 // And we'll look at all files in the jane austen directory
-let file = fs.readFileSync('files/cat.txt', 'utf8');
+let file = fs.readFileSync('files/dummies_coin.txt', 'utf8');
 //console.log(file);
 
 
